07b059190982c04921c62b4d5a5461e4f43ccc28
Shawn Trondillo <89569194+Strondillo@users.noreply.github.com>
2025-02-11 09:33:55 +0800

    Update-8

--- src/api/custom-user/routes/custom-user.ts
+++ src/api/custom-user/routes/custom-user.ts
@@ -1,36 +1,36 @@
 export default {
-    routes: [
+  routes: [
       {
-        method: 'POST',
-        path: '/custom-user/login',
-        handler: 'custom-user.login',
-        config: {
-          auth: false, // Make it accessible publicly
-        },
+          method: 'POST',
+          path: '/custom-user/login',
+          handler: 'custom-user.login',
+          config: {
+              auth: false,
+          },
       },
       {
-        method: 'POST',
-        path: '/custom-user/register',
-        handler: 'custom-user.register',
-        config: {
-          auth: false, // Make it accessible publicly
-        },
+          method: 'POST',
+          path: '/custom-user/register',
+          handler: 'custom-user.register',
+          config: {
+              auth: false,
+          },
       },
       {
-        method: 'GET',
-        path: '/custom-user', // Endpoint for fetching all users
-        handler: 'custom-user.find', // You can use Strapi's default `find` handler or a custom one
-        config: {
-          auth: false, // Adjust based on your security requirements
-        },
+          method: 'GET',
+          path: '/custom-user', // Fetch all users
+          handler: 'custom-user.find',
+          config: {
+              auth: false,
+          },
       },
       {
-        method: 'GET',
-        path: '/custom-user/:id', // Endpoint for fetching a specific user by ID
-        handler: 'custom-user.findOne', // You can use Strapi's default `findOne` handler or a custom one
-        config: {
-          auth: false, // Adjust based on your security requirements
-        },
+          method: 'GET',
+          path: '/custom-user/:id', // Fetch a specific user by ID
+          handler: 'custom-user.findOne',
+          config: {
+              auth: false,
+          },
       },
-    ],
-  };  
+  ],
+};
--- src/api/nutrition-assessment-category/routes/nutrition-assessment-category.ts
+++ src/api/nutrition-assessment-category/routes/nutrition-assessment-category.ts
@@ -2,6 +2,13 @@
  * nutrition-assessment-category router
  */
 
-import { factories } from '@strapi/strapi';
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreRouter('api::nutrition-assessment-category.nutrition-assessment-category');
+export default factories.createCoreRouter("api::nutrition-assessment-category.nutrition-assessment-category", {
+  config: {
+    update: {
+      auth: false, // Change to true if authentication is required
+      policies: [],
+    },
+  },
+});
